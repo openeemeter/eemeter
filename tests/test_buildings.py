625711bf7402ec83d65a16b189834c3bd20a7849
Phil Ngo <ngo.phil@gmail.com>
2014-10-23 17:54:53 -0500

    add tests to energybills,buildings

--- tests/test_buildings.py
+++ tests/test_buildings.py
@@ -3,43 +3,68 @@ from datetime import date, timedelta
 
 import pytest
 
+##### Fixtures #####
+
 @pytest.fixture(scope="module")
 def building_that_meets_criteria(request):
-    end_dates = [date(2012,1,1),
-                 date(2012,2,1),
-                 date(2012,3,1),
-                 date(2012,4,1),
-                 date(2012,5,1),
-                 date(2012,6,1),
-                 date(2012,7,1),
-                 date(2012,8,1),
-                 date(2012,9,1),
-                 date(2012,10,1),
-                 date(2012,11,1),
-                 date(2012,12,1),
-                 date(2013,1,1),
-                 date(2013,2,1),
-                 date(2013,3,1),
-                 date(2013,4,1),
-                 date(2013,5,1),
-                 date(2013,6,1),
-                 date(2013,7,1),
-                 date(2013,8,1),
-                 date(2013,9,1),
-                 date(2013,10,1),
-                 date(2013,11,1),
-                 date(2013,12,1)]
-    start_dates = [end_dates[0] - timedelta(days=30)]
-    start_dates.extend([end_date + timedelta(days=1) for end_date in end_dates[:-1]])
-    usages = [0,0,0,0,0,0,
-              0,0,0,0,0,0,
-              0,0,0,0,0,0,
-              0,0,0,0,0,0]
-
-    energy_bills = []
-    for usage,start_date,end_date in zip(usages,start_dates,end_dates):
-        energy_bills.append(EnergyBill(usage,start_date,end_date))
+    energy_bills = [EnergyBill(0,date(2012,1,1),date(2012,1,31)),
+                    EnergyBill(0,date(2012,2,1),date(2012,2,28)),
+                    EnergyBill(0,date(2012,3,1),date(2012,3,31)),
+                    EnergyBill(0,date(2012,4,1),date(2012,4,30)),
+                    EnergyBill(0,date(2012,5,1),date(2012,5,31)),
+                    EnergyBill(0,date(2012,6,1),date(2012,6,30)),
+                    EnergyBill(0,date(2012,7,1),date(2012,7,31)),
+                    EnergyBill(0,date(2012,8,1),date(2012,8,31)),
+                    EnergyBill(0,date(2012,9,1),date(2012,9,30)),
+                    EnergyBill(0,date(2012,10,1),date(2012,10,31)),
+                    EnergyBill(0,date(2012,11,1),date(2012,11,30)),
+                    EnergyBill(0,date(2012,12,1),date(2012,12,31)),
+                    EnergyBill(0,date(2013,1,1),date(2013,1,31)),
+                    EnergyBill(0,date(2013,2,1),date(2013,2,28)),
+                    EnergyBill(0,date(2013,3,1),date(2013,3,31)),
+                    EnergyBill(0,date(2013,4,1),date(2013,4,30)),
+                    EnergyBill(0,date(2013,5,1),date(2013,5,31)),
+                    EnergyBill(0,date(2013,6,1),date(2013,6,30)),
+                    EnergyBill(0,date(2013,7,1),date(2013,7,31)),
+                    EnergyBill(0,date(2013,8,1),date(2013,8,31)),
+                    EnergyBill(0,date(2013,9,1),date(2013,9,30)),
+                    EnergyBill(0,date(2013,10,1),date(2013,10,31)),
+                    EnergyBill(0,date(2013,11,1),date(2013,11,30)),
+                    EnergyBill(0,date(2013,12,1),date(2013,12,31))]
     return Building(energy_bills)
+
+@pytest.fixture(scope="module")
+def building_with_old_bills(request):
+    energy_bills = [EnergyBill(0,date(2012,8,1),date(2012,8,31)),
+                    EnergyBill(0,date(2012,9,1),date(2012,9,30)),
+                    EnergyBill(0,date(2012,10,1),date(2012,10,31)),
+                    EnergyBill(0,date(2012,11,1),date(2012,11,30)),
+                    EnergyBill(0,date(2012,12,1),date(2012,12,31)),
+                    EnergyBill(0,date(2012,1,1),date(2012,1,31)),
+                    EnergyBill(0,date(2012,2,1),date(2012,2,28)),
+                    EnergyBill(0,date(2012,3,1),date(2012,3,31)),
+                    EnergyBill(0,date(2012,4,1),date(2012,4,30)),
+                    EnergyBill(0,date(2012,5,1),date(2012,5,31)),
+                    EnergyBill(0,date(2012,6,1),date(2012,6,30)),
+                    EnergyBill(0,date(2012,7,1),date(2012,7,31)),
+                    EnergyBill(0,date(2012,8,1),date(2012,8,31)),
+                    EnergyBill(0,date(2012,9,1),date(2012,9,30)),
+                    EnergyBill(0,date(2012,10,1),date(2012,10,31)),
+                    EnergyBill(0,date(2012,11,1),date(2012,11,30)),
+                    EnergyBill(0,date(2012,12,1),date(2012,12,31))]
+    return Building(energy_bills)
+
+@pytest.fixture(scope="module")
+def building_with_too_few_bills(request):
+    return Building([])
+
+##### Tests #####
 
 def test_building_has_bills(building_that_meets_criteria):
     assert len(building_that_meets_criteria.energy_bills) >= 0
+
+def test_building_string(building_that_meets_criteria):
+    assert "Building (24 bills)" == str(building_that_meets_criteria)
+
+def test_building_meets_criteria(building_that_meets_criteria):
+    assert building_that_meets_criteria.meets_criteria()
--- tests/test_energy_bills.py
+++ tests/test_energy_bills.py
@@ -6,7 +6,7 @@ import pytest
 
 # Helper fixture
 @pytest.fixture(scope="module",
-                params=[0,1,1000,-1000])
+                params=[0,1,1000])
 def _energy_bill(request):
     usage = request.param
     start_date = None
@@ -16,24 +16,35 @@ def _energy_bill(request):
 @pytest.fixture(scope="module",
                 params=[(date(2014,1,1),date(2014,1,1),1),
                         (date(2014,1,1),date(2014,1,31),31)])
-def energy_bill_with_valid_date(request,_energy_bill):
+def valid_energy_bill(request,_energy_bill):
     _energy_bill.start_date, _energy_bill.end_date, days = request.param
     return _energy_bill,days
 
 @pytest.fixture(scope="module",
                 params=[(date(2014,1,1),date(2013,12,1))])
-def energy_bill_with_invalid_date(request,_energy_bill):
+def energy_bill_with_invalid_date_range(request,_energy_bill):
     _energy_bill.start_date, _energy_bill.end_date = request.param
     return _energy_bill
 
+@pytest.fixture(scope="module")
+def energy_bill_with_invalid_dates(_energy_bill):
+    return _energy_bill
+
+
 ##### Test cases #####
 
-def test_energy_bill_has_valid_date(energy_bill_with_valid_date):
-    energy_bill,days = energy_bill_with_valid_date
-    assert energy_bill.start_date <= \
-            energy_bill.end_date
-    assert energy_bill.days() == days
+def test_valid_energy_bill_is_valid(valid_energy_bill):
+    energy_bill,days = valid_energy_bill
+    assert energy_bill.is_valid()
 
-def test_energy_bill_has_invalid_date(energy_bill_with_invalid_date):
-    with pytest.raises(AssertionError):
-        energy_bill_with_invalid_date.days()
+def test_energy_bill_with_invalid_date_range_is_invalid(energy_bill_with_invalid_date_range):
+    assert not energy_bill_with_invalid_date_range.is_valid()
+
+def test_energy_bill_with_invalid_dates_is_invalid(energy_bill_with_invalid_dates):
+    assert not energy_bill_with_invalid_dates.is_valid()
+
+def test_energy_bill_string_is_correct(valid_energy_bill):
+    energy_bill,days = valid_energy_bill
+    assert "Energy Bill " in str(energy_bill)
+    assert "{}".format(energy_bill.end_date) in str(energy_bill)
+    assert "{}".format(energy_bill.usage) in str(energy_bill)
