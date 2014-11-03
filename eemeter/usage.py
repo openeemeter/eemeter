c1d08efea5cd461ace715bc612ef3514a7e45093
Phil Ngo <ngo.phil@gmail.com>
2014-11-03 10:56:07 -0600

    add fuel type

--- eemeter/usage.py
+++ eemeter/usage.py
@@ -1,8 +1,22 @@
+class FuelType:
+    def __init__(self,name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+electricity = FuelType("Electricity")
+natural_gas = FuelType("Natural gas")
 
 class Usage:
-    def __init__(self,usage,unit,start,end,estimated=False):
+    def __init__(self,usage,unit,fuel_type,start,end,estimated=False):
         self.usage = usage
         self.unit = unit
+        self.fuel_type = fuel_type
         self.start = start
         self.end = end
         self.estimated = estimated
+
+class UsageSet:
+    def __init__(self):
+        pass
--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,4 +1,7 @@
 from eemeter.usage import Usage
+from eemeter.usage import FuelType
+from eemeter.usage import electricity
+from eemeter.usage import natural_gas
 from eemeter.units import KilowattHour
 
 import arrow
@@ -9,9 +12,10 @@ import pytest
 ##### Fixtures #####
 
 @pytest.fixture(scope="module",
-                params=[(0,KilowattHour,arrow.get(2000,1,1).datetime,arrow.get(2000,1,31).datetime,False),
-                        (0.0,KilowattHour,arrow.get(2000,1,1).datetime,arrow.get(2000,1,31).datetime,False),
-                        (0,KilowattHour,arrow.get(2000,1,1).datetime,arrow.get(2000,1,31).datetime)])
+                params=[(0,KilowattHour,electricity,arrow.get(2000,1,1).datetime,arrow.get(2000,1,31).datetime,False),
+                        (0.0,KilowattHour,electricity,arrow.get(2000,1,1).datetime,arrow.get(2000,1,31).datetime,False),
+                        (0.0,KilowattHour,natural_gas,arrow.get(2000,1,1).datetime,arrow.get(2000,1,31).datetime,False),
+                        (0,KilowattHour,electricity,arrow.get(2000,1,1).datetime,arrow.get(2000,1,31).datetime)])
 def usage_zero_one_month(request):
     return Usage(*request.param)
 
@@ -20,6 +24,13 @@ def usage_zero_one_month(request):
 def test_usage_has_correct_attributes(usage_zero_one_month):
     assert usage_zero_one_month.usage == 0
     assert usage_zero_one_month.unit == KilowattHour
+    assert isinstance(usage_zero_one_month.fuel_type,FuelType)
     assert usage_zero_one_month.start == arrow.get(2000,1,1).datetime
     assert usage_zero_one_month.end == arrow.get(2000,1,31).datetime
     assert usage_zero_one_month.estimated == False
+
+def test_electricity():
+    assert "Electricity" == str(electricity)
+
+def test_natural_gas():
+    assert "Natural gas" == str(natural_gas)
