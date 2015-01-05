fix hdd/cdd swap error and add temperature sensitivity parameter computation

eemeter/meter/metrics.py
@@ -18,36 +18,6 @@ class RawAverageUsageMetric(MetricBase):
             return np.nan
         return np.mean([consumption.to(self.unit_name) for consumption in consumptions])
 
-class TemperatureRegressionParametersMetric(MetricBase):
-
-    # TODO - weight these by likelihood.
-    balance_points = range(55,70)
-
-    def __init__(self,unit_name,fuel_type):
-        self.fuel_type = fuel_type
-        self.unit_name = unit_name
-
-    def evaluate(self,consumption_history,weather_source):
-        consumptions = consumption_history.get(self.fuel_type)
-        usages = [c.to(self.unit_name) for c in consumptions]
-        avg_temps = weather_source.get_average_temperature(consumptions,"degF")
-        best_coeffs = None,None
-        best_r_value = -np.inf
-        for balance_point in self.balance_points:
-            u,t = self._filter_by_balance_point(balance_point,usages,avg_temps)
-            slope, intercept, r_value, p_value, std_err = stats.linregress(u,t)
-            if r_value > best_r_value and not np.isnan(p_value):
-                best_coeffs = slope,intercept
-        return best_coeffs
-
-    @staticmethod
-    def _filter_by_balance_point(balance_point,usages,avg_temps):
-        data = [(usage,avg_temp) for usage,avg_temp in zip(usages,avg_temps) if avg_temp >= balance_point]
-        if data:
-            return zip(*data)
-        else:
-            return [],[]
-
 class AverageTemperatureMetric(MetricBase):
     def __init__(self,fuel_type):
         self.fuel_type = fuel_type
@@ -124,9 +94,22 @@ class WeatherNormalizedAverageUsageMetric(MetricBase):
 
     def evaluate(self,consumption_history,temperature_sensitivity_parameters,weather_normal_source):
         consumptions = consumption_history.get(self.fuel_type)
-        usages = [c.to(self.unit_name) for c in consumptions]
-        normal_temps = weather_normal_source.get_average_temperature(consumptions,self.temperature_unit_name)
-        return None
+        normal_temps = weather_normal_source.get_daily_temperatures(self.temperature_unit_name)
+        return np.sum(self._estimate_usages(normal_temps,temperature_sensitivity_parameters))
+
+    @staticmethod
+    def _estimate_usages(temps,params):
+        ts_low,ts_high,base_load,bp_low,bp_diff = params
+        bp_high = bp_low + bp_diff
+        estimates = []
+        for temp in temps:
+            usage = base_load
+            if temp >= bp_high:
+                usage += ts_high * (temp - bp_high)
+            elif temp <= bp_low:
+                usage += ts_low * (bp_low - temp)
+            estimates.append(usage)
+        return estimates
 
 class TotalHDDMetric(MetricBase):
     def __init__(self,fuel_type):

tests/test_meter.py
@@ -5,7 +5,6 @@ from eemeter.consumption import natural_gas
 
 from eemeter.meter import MetricBase
 from eemeter.meter import RawAverageUsageMetric
-from eemeter.meter import TemperatureRegressionParametersMetric
 from eemeter.meter import AverageTemperatureMetric
 from eemeter.meter import WeatherNormalizedAverageUsageMetric
 from eemeter.meter import HDDCDDTemperatureSensitivityParametersMetric
@@ -243,21 +242,6 @@ def test_raw_average_usage_metric(consumption_history_one_year_electricity,
 
 @pytest.mark.slow
 @pytest.mark.internet
-def test_temperature_regression_parameters_metric(consumption_history_one_year_electricity,gsod_722880_2012_weather_source):
-    metric = TemperatureRegressionParametersMetric("kWh",electricity)
-    params = metric.evaluate(consumption_history_one_year_electricity,gsod_722880_2012_weather_source)
-
-    class MyMeter(Meter):
-        temperature_regression_metric = TemperatureRegressionParametersMetric("kWh",electricity)
-
-    meter = MyMeter()
-    results = meter.run(consumption_history=consumption_history_one_year_electricity,
-                        weather_source=gsod_722880_2012_weather_source)
-
-    assert results.temperature_regression_metric is not None
-
-@pytest.mark.slow
-@pytest.mark.internet
 def test_average_temperature_metric(consumption_history_one_year_electricity,
                                     gsod_722880_2012_weather_source):
     metric = AverageTemperatureMetric(electricity)
@@ -270,11 +254,21 @@ def test_average_temperature_metric(consumption_history_one_year_electricity,
 def test_weather_normalize(consumption_history_one_summer_electricity,
                            gsod_722880_2012_weather_source,
                            tmy3_722880_2012_weather_source):
-    metric = WeatherNormalizedAverageUsageMetric("kWh",fuel_type=electricity)
+    metric = WeatherNormalizedAverageUsageMetric("kWh",electricity)
     result = metric.evaluate(consumption_history_one_summer_electricity,
-                             gsod_722880_2012_weather_source,
+                             np.array([1,1,100,60,65]),
                              tmy3_722880_2012_weather_source)
-    assert False
+    assert abs(result - 37159.112829) < EPSILON
+
+def test_hdd_cdd_temperature_sensitivity_parameters_metric(consumption_history_one_year_electricity,gsod_722880_2012_weather_source):
+    metric = HDDCDDTemperatureSensitivityParametersMetric("kWh",fuel_type=electricity)
+    params = metric.evaluate(consumption_history_one_year_electricity,gsod_722880_2012_weather_source)
+    ts_low,ts_high,base_load,bp_low,bp_diff = params
+    assert abs(ts_low - 0) < EPSILON
+    assert abs(ts_high - 11.1939919 ) < EPSILON
+    assert abs(base_load - 1132.811176) < EPSILON
+    assert abs(bp_low - 55) < EPSILON
+    assert abs(bp_diff - 5) < EPSILON
 
 def test_fueltype_presence_flag(consumption_history_one_year_electricity,
                                 consumption_history_one_year_natural_gas):
@@ -447,14 +441,14 @@ def test_meter_stages(consumption_history_one_year_electricity,
                 {"temperature_sensitivity_parameters": HDDCDDTemperatureSensitivityParametersMetric("kWh",fuel_type=electricity)},
                 {"total_heating_degree_days":TotalHDDMetric(fuel_type=electricity),
                  "total_cooling_degree_days":TotalCDDMetric(fuel_type=electricity)},
-                {"normalized_usage": WeatherNormalizedAverageUsageMetric("kWh",fuel_type=electricity)}
-                ]
+                {"normalized_usage": WeatherNormalizedAverageUsageMetric("kWh",fuel_type=electricity)}]
     meter = MyMeter()
     result = meter.run(consumption_history=consumption_history_one_year_electricity,
             weather_source=gsod_722880_2012_weather_source,
             weather_normal_source=tmy3_722880_2012_weather_source)
     assert abs(result.total_heating_degree_days - 231.1) < EPSILON
     assert abs(result.total_cooling_degree_days - 2380.6) < EPSILON
+    assert abs(result.normalized_usage - 438588.6917814) < EPSILON
 
 def test_pre_post_metric():
     assert issubclass(PrePost,MetricBase)