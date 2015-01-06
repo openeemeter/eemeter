e7a26d219a9a2966e18446cb46a29f4a3d2a988e
Phil Ngo <ngo.phil@gmail.com>
2015-01-05 18:52:21 -0600

    add unused feature with realistic usage values

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -85,6 +85,34 @@ def consumption_history_one_year_natural_gas():
     return ConsumptionHistory(c_list)
 
 @pytest.fixture
+def consumption_history_long():
+    c_list = [Consumption(687600000,"J",electricity,datetime(2012,9,26),datetime(2012,10,24)),
+            Consumption(874800000,"J",electricity,datetime(2012,10,24),datetime(2012,11,21)),
+            Consumption(1332000000,"J",electricity,datetime(2012,11,21),datetime(2012,12,27)),
+            Consumption(1454400000,"J",electricity,datetime(2012,12,27),datetime(2013,1,29)),
+            Consumption(1155600000,"J",electricity,datetime(2013,1,29),datetime(2013,2,26)),
+            Consumption(1195200000,"J",electricity,datetime(2013,2,26),datetime(2013,3,27)),
+            Consumption(1033200000,"J",electricity,datetime(2013,3,27),datetime(2013,4,25)),
+            Consumption(752400000,"J",electricity,datetime(2013,4,25),datetime(2013,5,23)),
+            Consumption(889200000,"J",electricity,datetime(2013,5,23),datetime(2013,6,22)),
+            Consumption(3434400000,"J",electricity,datetime(2013,6,22),datetime(2013,7,26)),
+            Consumption(828000000,"J",electricity,datetime(2013,7,26),datetime(2013,8,22)),
+            Consumption(2217600000,"J",electricity,datetime(2013,8,22),datetime(2013,9,25)),
+            Consumption(680400000,"J",electricity,datetime(2013,9,25),datetime(2013,10,23)),
+            Consumption(1062000000,"J",electricity,datetime(2013,10,23),datetime(2013,11,22)),
+            Consumption(1720800000,"J",electricity,datetime(2013,11,22),datetime(2013,12,27)),
+            Consumption(1915200000,"J",electricity,datetime(2013,12,27),datetime(2014,1,30)),
+            Consumption(1458000000,"J",electricity,datetime(2014,1,30),datetime(2014,2,27)),
+            Consumption(1332000000,"J",electricity,datetime(2014,2,27),datetime(2014,3,29)),
+            Consumption(954000000,"J",electricity,datetime(2014,3,29),datetime(2014,4,26)),
+            Consumption(842400000,"J",electricity,datetime(2014,4,26),datetime(2014,5,28)),
+            Consumption(1220400000,"J",electricity,datetime(2014,5,28),datetime(2014,6,25)),
+            Consumption(1702800000,"J",electricity,datetime(2014,6,25),datetime(2014,7,25)),
+            Consumption(1375200000,"J",electricity,datetime(2014,7,25),datetime(2014,8,23)),
+            Consumption(1623600000,"J",electricity,datetime(2014,8,23),datetime(2014,9,25))]
+
+
+@pytest.fixture
 def consumption_history_overlapping_1():
     c_list = [Consumption(0,"thm",natural_gas,datetime(2011,1,1),datetime(2011,3,1)),
             Consumption(0,"thm",natural_gas,datetime(2011,2,28),datetime(2011,3,1))]
