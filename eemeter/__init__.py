e1ea1ee86a0bac9a4ff937dd3836b188c184187f
Phil Ngo <ngo.phil@gmail.com>
2015-01-20 13:27:28 -0600

    skip pint during setup

--- eemeter/__init__.py
+++ eemeter/__init__.py
@@ -1,10 +1,16 @@
-# Units
-from pint import UnitRegistry
-ureg = UnitRegistry()
-Q_ = ureg.Quantity
-
 # Version
 VERSION = (0, 0, 1)
 
 def get_version():
     return '{}.{}.{}'.format(VERSION[0], VERSION[1], VERSION[2])
+
+# Units
+try:
+    from pint import UnitRegistry
+    ureg = UnitRegistry()
+    Q_ = ureg.Quantity
+except ImportError:
+    import warnings
+    warnings.warn("Skipping pint to get version for setuptools.")
+    ureg = None
+    Q_ = None
