e198e3ac46cbd601df861e01c79cecc5b293c251
Phil Ngo <ngo.phil@gmail.com>
2015-01-28 12:44:07 -0600

    add pint to mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['numpy', 'scipy.optimize']
+MOCK_MODULES = ['numpy','pint','scipy.optimize']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
