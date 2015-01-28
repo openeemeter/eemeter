23d22575c724b671bdc3b83905a0fa65799e17e4
Phil Ngo <ngo.phil@gmail.com>
2015-01-28 12:39:32 -0600

    use recommended format for mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -15,11 +15,15 @@
 import sys
 import os
 
-import mock
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
 
 MOCK_MODULES = ['numpy', 'scipy.optimize']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
