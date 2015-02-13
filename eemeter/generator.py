70f9c0671d6958798eac3601f26696f4f17adc1b
Eric Potash <eric@k2co3.net>
2015-02-13 13:29:09 -0600

    portfolio generator

--- eemeter/generator.py
+++ eemeter/generator.py
@@ -1,4 +1,5 @@
 from consumption import Consumption
+import itertools
 
 class ConsumptionGenerator:
     def __init__(self, fuel_type, consumption_unit_name, weather_unit_name, heat_base, heat_sensitivity, cool_base, cool_sensitivity, daily_base_load):
@@ -33,3 +34,38 @@ class ConsumptionGenerator:
             consumptions.append(c)
 
         return consumptions
+    
+
+class ProjectGenerator:
+    def __init__(self, name, n_homes, fuel_type, consumption_unit_name, weather_unit_name, 
+               heat_base_dist, heat_sensitivity_dist, 
+               cool_base_dist, cool_sensitivity_dist,
+               daily_base_load_dist):
+        
+        self.name = name
+        self.n_homes = n_homes
+        self.fuel_type = fuel_type
+        self.consumption_unit_name = consumption_unit_name
+        
+        self.homes = []
+        
+        for i in range(n_homes):
+            c = ConsumptionGenerator(fuel_type, consumption_unit_name, weather_unit_name, 
+                                     heat_base_dist.rvs(), heat_sensitivity_dist.rvs(), 
+                                     cool_base_dist.rvs(), cool_sensitivity_dist.rvs(), 
+                                     daily_base_load_dist.rvs())
+            self.homes.append(c)
+            
+    def generate(self, weather_source, periods, noise=None):
+        return [h.generate(weather_source, periods, noise) for h in self.homes]
+    
+class PortfolioGenerator:
+    def __init__(self, name, projects):
+        self.name = name
+        self.projects = projects
+    
+    def generate(self, weather_sources, periods, noises=None):
+        if noises is None: noises = [None for p in projects]
+        return list(itertools.chain.from_iterable(
+            g.generate(w,p,n) for g,w,p,n in zip(self.projects, weather_sources, periods, noises)
+        ))
---
+++ scripts/generate_portfolio.py
@@ -0,0 +1,62 @@
+from eemeter import generator
+from datetime import datetime
+from eemeter.consumption import DatetimePeriod
+from eemeter.weather import TMY3WeatherSource
+
+import os
+import itertools
+from scipy.stats import uniform
+import pandas as pd
+import sys
+
+outdir = sys.argv[1]
+
+# initialize periods, projects, portolio
+periods1 = [DatetimePeriod(datetime(2011,i,1), datetime(2011,i+1,1)) for i in range(1,11)] # monthly 2011
+periods2 = [DatetimePeriod(datetime(2012,i,1), datetime(2012,i+1,1)) for i in range(1,11)] # monthly 2012
+
+project1 = generator.ProjectGenerator("Pasadena", 5, "electricity", "J", "degF", 
+                           uniform(loc=60, scale=10), uniform(loc=.5,scale=1), 
+                           uniform(loc=75, scale=10), uniform(loc=.5, scale=1),
+                           uniform(loc=5, scale=5))
+
+project2 = generator.ProjectGenerator("Chicago", 10, "gas", "J", "degF", 
+                           uniform(loc=60, scale=5), uniform(loc=.5,scale=1), 
+                           uniform(loc=0, scale=0), uniform(loc=0, scale=0),
+                           uniform(loc=5, scale=5))
+
+portfolio = generator.PortfolioGenerator("My Project", [project1, project2])
+
+# write projects csv
+projects = pd.DataFrame({'name': [p.name for p in portfolio.projects],
+              'fuel_type': [p.fuel_type for p in portfolio.projects],
+              'consumption_unit_name': [p.consumption_unit_name for p in portfolio.projects]})
+projects.index.name = 'id'
+
+projects.to_csv(os.path.join(outdir, 'projects.csv'))
+
+# write customers csv
+project_customer_ids = []
+project_ids = []
+
+for i in range(len(portfolio.projects)):
+    for j in range(portfolio.projects[i].n_homes):
+        project_ids.append(i)
+        project_customer_ids.append(j)
+    
+customers = pd.DataFrame({'project_customer_id': project_customer_ids, 'project_id' : project_ids})
+customers.index.name = 'id'
+
+customers.to_csv(os.path.join(outdir, 'customers.csv'))
+
+# initialize weather
+pasadena = TMY3WeatherSource('722880',os.environ.get("TMY3_DIRECTORY"))
+chicago = TMY3WeatherSource('725300',os.environ.get("TMY3_DIRECTORY"))
+
+consumptions = portfolio.generate([pasadena, chicago], [periods1, periods2], [None, None])
+
+c = list(itertools.chain.from_iterable([[id, c.start, c.end, c.to("J")] for c in consumptions[id]] for id in range(len(consumptions))))
+
+consumption = pd.DataFrame(c, columns=['customer_id', 'start', 'end', 'joules'])
+consumption.index.name = 'id'
+consumption.to_csv(os.path.join(outdir, 'consumption.csv'))
