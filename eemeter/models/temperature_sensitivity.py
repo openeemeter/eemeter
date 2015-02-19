301d0aa16a0361afcee3bd2958956948a2ca3267
Phil Ngo <ngo.phil@gmail.com>
2015-02-19 16:46:29 -0600

    add more detailed docstrings for models

--- eemeter/models/temperature_sensitivity.py
+++ eemeter/models/temperature_sensitivity.py
@@ -7,6 +7,11 @@ class ModelBase(object):
         self.bounds = bounds
 
     def parameter_optimization(self,average_daily_usages,observed_daily_temps):
+        """Returns parameters which, according to an optimization routine in
+        `scipy.optimize`, minimize the sum of squared errors between observed
+        usages and the output of the a model which takes observed_daily_temps
+        and returns usage estimates.
+        """
         def objective_function(params):
             usages_est = self.compute_usage_estimates(params,observed_daily_temps)
             return np.sum((average_daily_usages - usages_est)**2)
@@ -17,10 +22,36 @@ class ModelBase(object):
         params = result.x
         return params
 
+    @staticmethod
+    def compute_usage_estimates(params,observed_daily_temps):
+        """Return usage estimates given model parameters and temperature
+        observations as an iterable of iterables containing daily temperatures
+        (e.g. [[70.1,71.7,46.3],[10.1,15.1,16.0]]). Content and format of
+        `params` must be determined by inheriting classes. Function must be
+        implemented by all inheriting classes.
+        """
+        raise NotImplementedError
+
 class HDDCDDBalancePointModel(ModelBase):
 
     @staticmethod
     def compute_usage_estimates(params,observed_daily_temps):
+        """Returns usage estimates for a combined, dual balance point,
+        heating/cooling degree day model. Parameters are given in the form
+        `params=(ts_low,ts_high,base_load,bp_low,bp_diff)`, in which:
+
+            - `ts_low` is the (generally positive) temperature sensitivity
+            (units: usage per hdd) beyond the lower (heating degree day)
+            balance point
+            - `ts_high` is the (generally positive) temperature sensitivity
+            (units: usage per cdd) beyond the upper (cooling degree day balance
+            point
+            - `base_load` is the daily non-temperature-related usage
+            - `bp_low` is the reference temperature of the lower (hdd) balance
+            point
+            - bd_diff is the (generally positive) difference between the
+            implicitly defined `bp_high` and bp_low
+        """
         # get parameters
         ts_low,ts_high,base_load,bp_low,bp_diff = params
         bp_high = bp_low + bp_diff
@@ -52,6 +83,19 @@ class HDDBalancePointModel(ModelBase):
 
     @staticmethod
     def compute_usage_estimates(params,observed_daily_temps):
+        """Returns usage estimates for a simple single balance point, heating
+        degree day model. Parameters are given in the form
+        `params=(reference_temperature,base_level_consumption,heating_slope)`,
+        in which:
+
+            - `reference_temperature` is the temperature base of the heating
+            degree day balance point
+            - `base_level_consumption` is the daily non-temperature-related
+            usage
+            - `heating_slope` is the (generally positive) temperature
+            sensitivity (units: usage per hdd) beyond the heating degree day
+            reference temperature
+        """
         # get parameters
         reference_temperature,base_level_consumption,heating_slope = params
 
@@ -76,6 +120,19 @@ class CDDBalancePointModel(ModelBase):
 
     @staticmethod
     def compute_usage_estimates(params,observed_daily_temps):
+        """Returns usage estimates for a simple single balance point, heating
+        degree day model. Parameters are given in the form
+        `params=(reference_temperature,base_level_consumption,cooling_slope)`,
+        in which:
+
+            - `reference_temperature` is the temperature base of the cooling
+            degree day balance point
+            - `base_level_consumption` is the daily non-temperature-related
+            usage
+            - `cooling_slope` is the (generally positive) temperature
+            sensitivity (units: usage per cdd) beyond the cooling degree day
+            reference temperature
+        """
         # get parameters
         reference_temperature,base_level_consumption,cooling_slope = params
 
