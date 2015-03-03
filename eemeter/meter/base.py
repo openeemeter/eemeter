4601536c0d73827c85b4e6e5da17efcc8ee7807a
Phil Ngo <ngo.phil@gmail.com>
2015-03-03 12:18:16 -0600

    coding style

--- eemeter/meter/base.py
+++ eemeter/meter/base.py
@@ -149,8 +149,7 @@ class TemperatureSensitivityParameterOptimizationMeter(MeterBase):
         consumptions = consumption_history.get(self.fuel_type)
         average_daily_usages = [c.average_daily_usage(self.fuel_unit_str) for c in consumptions]
         observed_daily_temps = weather_source.get_daily_temperatures(consumptions,self.temperature_unit_str)
-        weights = [c.timedelta.days for c in consumptions
-                   ]
+        weights = [c.timedelta.days for c in consumptions]
         params = self.model.parameter_optimization(average_daily_usages,observed_daily_temps, weights)
 
         n_daily_temps = np.array([len(temps) for temps in observed_daily_temps])
