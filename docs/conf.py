e38f770d260f917f33c51c4cb9f400f26c258240
Phil Ngo <ngo.phil@gmail.com>
2015-03-16 11:26:12 -0500

    use napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'numpydoc',
+    'sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
