766031c3df6b61d735232845a52ca778bcd91426
Phil Ngo <ngo.phil@gmail.com>
2015-03-16 11:42:04 -0500

    use numpydoc

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
