ac07d8fefa091d7ceebcdf98a387d7813af9a8b5
Phil Ngo <ngo.phil@gmail.com>
2015-03-19 12:39:20 -0500

    add green button import support

--- eemeter/importers.py
+++ eemeter/importers.py
@@ -2,6 +2,7 @@ from lxml import etree
 from dateutil.parser import parse
 from eemeter.consumption import Consumption
 from eemeter.consumption import ConsumptionHistory
+from datetime import datetime
 
 hpxml_fuel_type_mapping = {
     "electricity": "electricity",
@@ -38,4 +39,31 @@ def import_hpxml(filename):
                 estimated = (reading_type[0].text == "estimated")
             consumption = Consumption(usage,unit_str,fuel_type,start,end,estimated)
             consumptions.append(consumption)
+    return ConsumptionHistory(consumptions)
+
+def import_green_button_xml(filename):
+    with(open(filename,'r')) as f:
+        tree = etree.parse(f)
+
+    interval_blocks = tree.xpath("//*[local-name() = 'IntervalBlock']")
+
+    def get_consumption(reading):
+        usage = int(reading.xpath("*[local-name() = 'value']")[0].text)
+        fuel_type = "electricity"
+        unit_str = "Wh"
+        time_period = reading.xpath("*[local-name() = 'timePeriod']")[0]
+        start_ms = int(time_period.xpath("*[local-name() = 'start']")[0].text)
+        duration_ms = int(time_period.xpath("*[local-name() = 'duration']")[0].text)
+        end_ms = start_ms + duration_ms
+        start = datetime.fromtimestamp(start_ms)
+        end = datetime.fromtimestamp(end_ms)
+        return Consumption(usage,unit_str,fuel_type,start,end)
+
+    consumptions = []
+    for block in interval_blocks:
+        readings = block.xpath("*[local-name() = 'IntervalReading']")
+        for reading in readings:
+            consumption = get_consumption(reading)
+            consumptions.append(consumption)
+
     return ConsumptionHistory(consumptions)
