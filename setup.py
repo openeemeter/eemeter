badec43f063ef32922d81dc50af94eb265a16b51
Phil Ngo <ngo.phil@gmail.com>
2015-04-24 15:27:12 -0500

    normalize names

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 version = __import__('eemeter').get_version()
 
-setup(name='OpenEEMeter',
+setup(name='eemeter',
     version=version,
     description='Open Energy Efficiency Meter',
     author='Matt Gee, Phil Ngo, Eric Potash',
