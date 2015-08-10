51c511ed7cd3e026f7f322bd7a3cc5609a92249e
Phil Ngo <ngo.phil@gmail.com>
2015-08-10 10:17:33 -0500

    remove shortcuts

--- eemeter/__init__.py
+++ eemeter/__init__.py
@@ -14,11 +14,3 @@ except ImportError:
     warnings.warn("Skipping pint to get version for setuptools.")
     ureg = None
     Q_ = None
-
-from eemeter.evaluation import Period
-from eemeter.consumption import ConsumptionData
-from eemeter.project import Project
-from eemeter.config.yaml_parser import load
-from eemeter.meter.base import DataContainer
-from eemeter.meter.base import DataCollection
-
