03ebbe5d68d3302dc3e3cf4870431f59955074d9
Phil Ngo <ngo.phil@gmail.com>
2015-09-14 16:55:35 -0500

    Add coverage

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
 
 version = __import__('eemeter').get_version()
 
 long_description = "Standard methods for calculating energy efficiency savings."
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        import sys
+        errno = subprocess.call([sys.executable, 'runtests.py', '--runslow', '--cov=eemeter'])
+        raise SystemExit(errno)
 
 setup(name='eemeter',
     version=version,
@@ -17,6 +31,7 @@ setup(name='eemeter',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
     ],
+    cmdclass = {'test': PyTest},
     keywords='open energy efficiency meter method methods calculation savings',
     packages=find_packages(),
     install_requires=['pint',
