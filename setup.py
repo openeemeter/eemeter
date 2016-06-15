39fcc4740d76e8906c44e8b6927b183c04570731
Phil Ngo <ngo.phil@gmail.com>
2016-06-15 13:55:00 -0700

    Fix setup.py flake errors

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup, find_packages, Command
 
 version = __import__('eemeter').get_version()
 
-long_description = "Standard methods for calculating energy efficiency savings."
+long_description = (
+    "Standard methods for calculating energy efficiency savings."
+)
+
 
 class PyTest(Command):
+
     user_options = []
+
     def initialize_options(self):
         pass
 
@@ -15,16 +20,20 @@ class PyTest(Command):
     def run(self):
         import subprocess
         import sys
-        errno = subprocess.call([sys.executable, 'runtests.py', '--runslow', '--cov-report', 'term-missing', '--cov', 'eemeter'  ])
+        errno = subprocess.call([
+            sys.executable, 'runtests.py', '--runslow',
+            '--cov-report', 'term-missing', '--cov', 'eemeter'
+        ])
         raise SystemExit(errno)
 
 
-setup(name='eemeter',
+setup(
+    name='eemeter',
     version=version,
     description='Open Energy Efficiency Meter',
     long_description=long_description,
     url='https://github.com/impactlab/eemeter/',
-    author='Matt Gee, Phil Ngo, Eric Potash',
+    author='Matt Gee, Phil Ngo',
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -32,16 +41,18 @@ setup(name='eemeter',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
     ],
-    cmdclass = {'test': PyTest},
-    keywords='open energy efficiency meter method methods calculation savings',
+    cmdclass={'test': PyTest},
+    keywords='open energy efficiency meter',
     packages=find_packages(),
-    install_requires=['pint',
-                      'pyyaml',
-                      'scipy',
-                      'lxml',
-                      'numpy',
-                      'pandas >= 0.18',
-                      'requests',
-                      'pytz'],
-    package_data={'': ['*.json','*.gz']},
+    install_requires=[
+        'pint',
+        'pyyaml',
+        'scipy',
+        'lxml',
+        'numpy',
+        'pandas >= 0.18',
+        'requests',
+        'pytz'
+    ],
+    package_data={'': ['*.json', '*.gz']},
 )
