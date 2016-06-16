d961d6cf4462a7e90039177d73cc88982bd9563d
Phil Ngo <ngo.phil@gmail.com>
2016-06-16 16:16:40 -0700

    Add sqlite json store basic usage test

---
+++ tests/weather/test_sqlite_json_store.py
@@ -0,0 +1,23 @@
+from eemeter.weather.cache import SqliteJSONStore
+import tempfile
+import pytest
+
+
+def test_basic_usage():
+    tmpdir = tempfile.mkdtemp()
+    s = SqliteJSONStore(tmpdir)
+
+    assert s.key_exists("a") is False
+    data = s.retrieve_json("a")
+    assert data is None
+
+    s.save_json("a", {"b": [1, "two", 3.0]})
+    assert s.key_exists("a") is True
+    data = s.retrieve_json("a")
+    assert len(data["b"]) == 3
+    assert data["b"][0] == 1
+    assert data["b"][1] == "two"
+    assert data["b"][2] == 3.0
+
+    s.clear()
+    assert s.key_exists("a") is False
