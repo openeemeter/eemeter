c3dcae813d74645a4cfe5c45e6d6b17b8852cef4
Phil Ngo <ngo.phil@gmail.com>
2016-06-21 11:02:12 -0700

    Test bad zip

--- tests/processors/test_get_weather_source.py
+++ tests/processors/test_get_weather_source.py
@@ -19,14 +19,41 @@ def project():
     return project
 
 
+@pytest.fixture
+def project_bad_zip():
+    ets = EnergyTraceSet({})
+    interventions = []
+    site = ZIPCodeSite("00000")
+    project = Project(ets, interventions, site)
+    return project
+
+
 def test_basic_usage(project):
 
     lc = LogCollector()
 
     with lc.collect_logs("get_weather_source") as logger:
         ws = get_weather_source(logger, project)
+
     assert ws.station == '722880'
 
     logs = lc.items["get_weather_source"].splitlines()
     assert "INFO - Mapped ZIP code 91104 to ISD station 722880" in logs[0]
     assert "INFO - Created ISDWeatherSource using station 722880" in logs[1]
+
+
+
+def test_bad_zip(project_bad_zip):
+
+    lc = LogCollector()
+
+    with lc.collect_logs("get_weather_source") as logger:
+        ws = get_weather_source(logger, project_bad_zip)
+
+    assert ws is None
+
+    logs = lc.items["get_weather_source"].splitlines()
+    assert "INFO - Mapped ZIP code 00000 to ISD station None" in logs[0]
+    assert (
+        "ERROR - Could not create ISDWeatherSource for station None" in logs[1]
+    )
