2f1b554b75d72ab8efea0fd85e3f1eedd69b6939
Phil Ngo <ngo.phil@gmail.com>
2016-09-13 18:26:51 -0700

    Attribution and copyright changes

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Open Energy Efficiency Meter'
-copyright = u'2015, The Impact Lab Co.'
+copyright = u'2016, Open Energy Efficiency Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -265,7 +265,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'eemeter', u'Open Energy Efficiency Meter Documentation',
-     [u'Phil Ngo'], 1)
+     [u'Open Energy Efficiency'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -279,7 +279,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'eemeter', u'Open Energy Efficiency Meter Documentation',
-   u'Phil Ngo', 'eemeter', 'Standard methods for calculating energy efficiency savings.',
+   u'Open Energy Efficiency', 'eemeter', 'Standard methods for calculating energy efficiency savings.',
    'Miscellaneous'),
 ]
 
