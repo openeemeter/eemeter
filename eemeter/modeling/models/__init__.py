293497088ebd3277eaab6678b8a7e88ca9542383
Phil Ngo <ngo.phil@gmail.com>
2016-09-21 11:17:49 -0700

    Add shortcut to model

--- eemeter/modeling/models/__init__.py
+++ eemeter/modeling/models/__init__.py
@@ -1,3 +1,7 @@
 from eemeter.modeling.models.seasonal import SeasonalElasticNetCVModel
+from eemeter.modeling.models.billing import BillingElasticNetCVModel
 
-__all__ = ['SeasonalElasticNetCVModel', ]
+__all__ = (
+    'SeasonalElasticNetCVModel',
+    'BillingElasticNetCVModel',
+)
