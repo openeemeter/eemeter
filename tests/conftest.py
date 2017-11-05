bc61d555cb7c4e210b4fd220481251baf5cdc11e
Michael Floering <michaelfloering@gmail.com>
2017-11-05 14:45:07 -0800

    Test suite: Disable file logging.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,8 @@
 
 py.test docs: https://docs.pytest.org/en/2.7.3/plugins.html
 """
-import logging
-from eemeter.log import setup_logging
+from eemeter.log import setup_logging, disable_file_logging
 
-def pytest_runtest_setup(item):
-    setup_logging(eemeter_log_level=logging.DEBUG, allow_console_logging=True)
+def pytest_configure():
+    setup_logging(eemeter_log_level='DEBUG', allow_console_logging=True)
+    disable_file_logging()
