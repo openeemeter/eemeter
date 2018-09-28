8e8769bd88bc6e9cf7c917e79a9dc287b3dbe242
Stephen Suffian <steve@openee.io>
2018-09-28 13:09:32 -0400

    fixed typo on test

--- tests/test_derivatives.py
+++ tests/test_derivatives.py
@@ -174,7 +174,7 @@ def test_metered_savings_cdd_hdd_billing_no_reporting_data(
 @pytest.fixture
 def reporting_meter_data_billing_single_record():
     index = pd.date_range("2011-01-01", freq="MS", periods=13, tz="UTC")
-    return pd.DataFrame({"value": None}, index=index)[:0]
+    return pd.DataFrame({"value": None}, index=index)[:1]
 
 
 def test_metered_savings_cdd_hdd_billing_single_record_reporting_data(
