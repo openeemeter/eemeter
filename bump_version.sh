f3723e4dad03d49c5bbb0791d3df252c564aac8b
Phil Ngo <ngo.phil@gmail.com>
2019-01-03 13:15:09 -0800

    Bump pipfile and add bump version helper

---
+++ bump_version.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e  # fail script on any error, show commands
+
+OLD_VERSION=$1  # e.g., 0.0.0
+NEW_VERSION=$2  # e.g., 0.0.1
+NEW_VERSION_LENGTH=$(printf "%s" "$NEW_VERSION" | wc -c)
+DASHES=$(printf "%${NEW_VERSION_LENGTH}s" | sed 's/ /-/g')
+
+echo "git checkout master"
+echo "git pull"
+echo "git checkout -b release/v${NEW_VERSION}"
+echo ""
+echo "sed -i -e 's/${OLD_VERSION}/${NEW_VERSION}/g' eemeter/__version__.py"
+echo "sed -i -e '/Development/,/-----------/ c\\
+Development\\
+-----------\\
+\\
+* Placeholder\\
+\\
+${NEW_VERSION}\\
+${DASHES}\\
+' CHANGELOG.md"
+echo "rm -f eemeter/__version__.py-e"
+echo "rm -f CHANGELOG.md-e"
+echo ""
+echo "git checkout master"
+echo "git pull"
+echo "git checkout -b release/v${NEW_VERSION}"
+echo "git commit -am \"Bump version\""
+echo "git tag v${NEW_VERSION}"
+echo "git push -u origin release/v${NEW_VERSION} --tags"
+echo "git checkout master"
+echo "git pull"
+echo "git merge release/v${NEW_VERSION}"
+echo "git push"
