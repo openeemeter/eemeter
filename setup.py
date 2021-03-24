8482e79f0cb6f64164dafbf1b9883765ca531998
Steve <steve@recurve.com>
2021-03-24 13:12:13 -0400

    Removed py35, added py38, and fixed test

--- setup.py
+++ setup.py
@@ -95,9 +95,9 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     # $ setup.py publish support.
     cmdclass={"upload": UploadCommand},
