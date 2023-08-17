ff97f6deb10295a61a618ced3eda8f71168a1c22
root <joydeep@recurve.com>
2023-08-17 14:38:33 +0000

    Fixed full model function finder removing globals
    Signed-off-by: root <joydeep@recurve.com>
    Signed-off-by: joydeep-recurve <joydeep@recurve.com>

--- eemeter/caltrack/daily/base_models/full_model_import_finder.py
+++ eemeter/caltrack/daily/base_models/full_model_import_finder.py
@@ -3,30 +3,38 @@ import importlib
 import time
 import numpy as np
 
-# Define a global variable to store the full_model function
-full_model = None
+
+def get_arch_type():
+    """
+    Get the machine's processor architecture and return the appropriate architecture type.
+    """
+    arch = platform.machine()
+    if arch == "x86_64":
+        return "x86"
+    elif arch == "aarch64":
+        return "arm"
+    else:
+        return "other"
+
 
 # Define a function to initialize the full_model function
 def initialize_full_model():
-    global full_model
 
-    # Get the machine's processor architecture
-    arch = platform.machine()
+    # Get the machine's processor architecture to use in identifying the binary to import
+    arch_type = get_arch_type()
 
-    if arch == 'x86_64':
-        arch_type = "x86"
-    elif arch == 'aarch64':
-        arch_type = "arm"
-    else:
-        arch_type = "other"
     # Import the Python version of full_model
-    full_model_python = importlib.import_module("eemeter.caltrack.daily.base_models.full_model").full_model
+    full_model_python = importlib.import_module(
+        "eemeter.caltrack.daily.base_models.full_model"
+    ).full_model
 
     # Import the Cython version of full_model if present and performs faster, else default to the Python version
     try:
-        full_model_cpp = importlib.import_module("bin." + arch_type + ".full_model_ext").full_model_wrapper
+        full_model_cpp = importlib.import_module(
+            "bin." + arch_type + ".full_model_ext"
+        ).full_model_wrapper
 
-        loop_count = 100
+        loop_count = 0
         hdd_bp = 50
         hdd_beta = 0.01
         hdd_k = 0.001
@@ -44,13 +52,25 @@ def initialize_full_model():
         py_times = []
 
         # Compile Numba for fair comparison
-        full_model_python(hdd_bp,hdd_beta, hdd_k, cdd_bp, cdd_beta, cdd_k, intercept, T_fit_bnds, T)
+        full_model_python(
+            hdd_bp, hdd_beta, hdd_k, cdd_bp, cdd_beta, cdd_k, intercept, T_fit_bnds, T
+        )
 
         for i in range(10):
             start = time.process_time()
 
             for i in range(loop_count):
-                full_model_cpp(hdd_bp,hdd_beta, hdd_k, cdd_bp, cdd_beta, cdd_k, intercept, T_fit_bnds, T)
+                full_model_cpp(
+                    hdd_bp,
+                    hdd_beta,
+                    hdd_k,
+                    cdd_bp,
+                    cdd_beta,
+                    cdd_k,
+                    intercept,
+                    T_fit_bnds,
+                    T,
+                )
 
             cpp_time = time.process_time() - start
             cpp_times.append(cpp_time)
@@ -58,7 +78,17 @@ def initialize_full_model():
             start = time.process_time()
 
             for i in range(loop_count):
-                full_model_python(hdd_bp,hdd_beta, hdd_k, cdd_bp, cdd_beta, cdd_k, intercept, T_fit_bnds, T)
+                full_model_python(
+                    hdd_bp,
+                    hdd_beta,
+                    hdd_k,
+                    cdd_bp,
+                    cdd_beta,
+                    cdd_k,
+                    intercept,
+                    T_fit_bnds,
+                    T,
+                )
 
             py_time = time.process_time() - start
             py_times.append(py_time)
@@ -71,8 +101,12 @@ def initialize_full_model():
         else:
             full_model = full_model_python
 
-    except ImportError:
+    except:
         full_model = full_model_python
+    
+    return full_model
+        
+
 
 # Call the initialize_full_model function to initialize the full_model function
-initialize_full_model()
+full_model = initialize_full_model()
