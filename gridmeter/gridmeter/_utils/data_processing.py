fbae3cea051925e3b2116d18cd83fa03d0b10965
joydeep-recurve <joydeep@recurve.com>
2023-12-07 20:25:54 +0000

    Fixed Month time period errors, other stability fixes
    Signed-off-by: joydeep-recurve <joydeep@recurve.com>

--- gridmeter/gridmeter/_utils/data_processing.py
+++ gridmeter/gridmeter/_utils/data_processing.py
@@ -2,13 +2,14 @@ from gridmeter._utils.data_processing_settings import Data_Settings
 from gridmeter._utils import const as _const
 import pandas as pd
 import numpy as np
+import time
 
-
-# TODO: LOADSHAPE_Type should be None if inputing loadshape just like TIME_PERIOD
 
 # TODO: Need to think through interpolate_missing with loadshape input vs time_series
     # - Time Series: 
     #     - check that datetime is continuous for given input
+    #     - create a new dataframe with id's and correct time column
+    #     - join on new df and old
     #     - interpolate nan values
 
     # - Loadshape:
@@ -17,14 +18,6 @@ import numpy as np
     #     - join on new df and old
     #     - interpolate nan values
 
-
-# TODO: Min data should probably if interpolation is set to true
-
-# TODO: Need a way to combine Data classes into a single Data class
-
-# TODO: what to do with empty ls/ts/feature dataframes? (if dataframe is empty, then it should be None)
-
-# TODO: if TIME_PERIOD is month, then minimum check doesn't work as is
 
 class Data:
     def __init__(self, settings: Data_Settings | None = None):
@@ -38,6 +31,24 @@ class Data:
 
         # TODO: let's make id the index
         self.excluded_ids = pd.DataFrame(columns=["id", "reason"])
+
+
+    def extend(self, other):
+        """
+            Extend the current Data instance with the Data instance(s) in other by concatenating the features and loadshape dataframes.
+        """
+        if not isinstance(other, list):
+            other = [other]
+
+        for data_instance in other:
+            if isinstance(data_instance, Data):
+                if self.features is not None and data_instance.features is not None:
+                    self.features = pd.concat([self.features, data_instance.features])
+                if self.loadshape is not None and data_instance.loadshape is not None:
+                    self.loadshape = pd.concat([self.loadshape, data_instance.loadshape])
+            else:
+                raise TypeError("All elements in other must be instances of Data")
+            
 
     def _find_groupby_columns(self) -> list:
         """
@@ -62,23 +73,23 @@ class Data:
     def _add_index_columns_from_datetime(self, df: pd.DataFrame) -> pd.DataFrame:
         # Add hour column
         if "hour" in self.settings.TIME_PERIOD:
-            df["hour"] = df.index.hour
+            df["hour"] = df['datetime'].dt.hour
 
         # Add month column
         if "month" in self.settings.TIME_PERIOD:
-            df["month"] = df.index.month
+            df["month"] = df['datetime'].dt.month
 
         # Add day_of_week column
         if "day_of_week" in self.settings.TIME_PERIOD:
-            df["day_of_week"] = df.index.dayofweek
+            df["day_of_week"] = df['datetime'].dt.dayofweek
 
         # Add day_of_year column
         if "day_of_year" in self.settings.TIME_PERIOD:
-            df["day_of_year"] = df.index.dayofyear
+            df["day_of_year"] = df['datetime'].dt.dayofyear
 
         # Add weekday_weekend column
         if "weekday_weekend" in self.settings.TIME_PERIOD:
-            df["weekday_weekend"] = df.index.dayofweek
+            df["weekday_weekend"] = df['datetime'].dt.dayofweek
 
             # Setting the ordering to weekday, weekend
             df["weekday_weekend"] = (
@@ -89,7 +100,7 @@ class Data:
 
         # Add season column
         if "season" in self.settings.TIME_PERIOD:
-            df["season"] = df.index.month.map(self.settings.SEASON._NUM_DICT).map(
+            df["season"] = df['datetime'].dt.month.map(self.settings.SEASON._NUM_DICT).map(
                 self.settings.SEASON._ORDER
             )
 
@@ -109,14 +120,19 @@ class Data:
         # loadshape df has the "time" column, whereas timeseries df has the "datetime" column
         subset_columns = expected_columns[:-1]
 
-        # TODO: What to do with duplicates?
-        df = df.drop_duplicates(subset=subset_columns, keep="first")
+        # TODO : To eliminate duplicates, find the absolute minimum value of loadshape
+        # The below command takes 20x time to execute than the one below it !!!!!
+
+        # create another dataframe having the duplicates
+        # df['abs_loadshape'] = df['loadshape'].abs()
+        # df = df.loc[df.groupby(subset_columns)['abs_loadshape'].idxmin()]
+
+        df.drop_duplicates(subset=subset_columns, keep="first", inplace = True)
 
         # Check that the minimum time counts per id is consistent for the input loadshape_df
         unique_time_counts = df["time"].nunique()
         unique_time_counts_per_id = df.groupby("id")["time"].nunique()
 
-        # TODO: We need to throw an error if TS not given and TIME_PERIOD is set
         if self.settings.INTERPOLATE_MISSING:
             if self.settings.TIME_PERIOD is None:
                 # for loadshape type dataframe
@@ -160,10 +176,9 @@ class Data:
                             [self.excluded_ids, excluded_ids], ignore_index=True
                         )
 
-            # TODO: Getting lots of deprecation warnings here (refer to Tutorial)
             # Fill NaN values with interpolation
-            df = (
-                df.groupby("id")
+            df['loadshape'] = (
+                df.groupby("id")['loadshape']
                 .apply(lambda x: x.interpolate(method="linear", limit_direction="both"))
                 .reset_index(drop=True)
             )
@@ -183,7 +198,7 @@ class Data:
                     }
                 )
                 self.excluded_ids = pd.concat(
-                    [self.excluded_ids, invalid_ids], ignore_index=True
+                    [self.excluded_ids, invalid_ids_df], ignore_index=True
                 )
 
             else:
@@ -225,7 +240,7 @@ class Data:
         df = df.dropna()
 
         # TODO: What to do with duplicates?
-        df = df.drop_duplicates(keep="first")
+        df = df.drop_duplicates(keep="first" , inplace = True)
 
         # drop any ids that are in excluded_ids from loadshape (or init)
         df = df[~df["id"].isin(self.excluded_ids["id"])]
@@ -274,19 +289,30 @@ class Data:
         # Remove duplicates
         subset_columns = expected_columns[:-1]
 
-        # TODO: What to do with duplicates?
+        # TODO: To eliminate duplicates, find the absolute minimum value of loadshape
+        # The below command takes 20x time to execute than the one below it !!!!!
+        # base_df['abs_' + df_type] = base_df[df_type].abs()
+        # base_df = base_df.loc[base_df.groupby(subset_columns)['abs_' + df_type].idxmin()] 
+
         base_df = base_df.drop_duplicates(subset=subset_columns, keep="first")
+
+        base_df = self._add_index_columns_from_datetime(base_df) # Add month / day_of_week / hour / etc columns
 
         # Check that each id has a minimum granularity lower than requested time period, otherwise we cannot aggregate
         # get minimum time interval per id
         base_df["time_diff"] = base_df.groupby("id")["datetime"].diff()
         min_time_diff_per_id = base_df.groupby("id")["time_diff"].min() / np.timedelta64(1, 'm')
 
-        # TODO: this doesn't work well with months (refer to Tutorial)
         # Get the ids that have a higher minimum granularity than defined
-        invalid_ids = min_time_diff_per_id[
-            min_time_diff_per_id > _const.min_granularity_per_time_period[self.settings.TIME_PERIOD]
-        ].index.tolist()
+        if self.settings.TIME_PERIOD != 'month':
+            invalid_ids = min_time_diff_per_id[
+                min_time_diff_per_id > _const.min_granularity_per_time_period[self.settings.TIME_PERIOD]
+            ].index.tolist()
+
+        else:
+            # Check that every ID has 12 months available.
+            unique_month_counts_per_id = base_df.groupby('id')['month'].nunique()
+            invalid_ids = unique_month_counts_per_id[unique_month_counts_per_id < 12].index.tolist()
 
         # Remove the invalid ids from the base_df
         base_df = base_df[~base_df["id"].isin(invalid_ids)]        
@@ -305,24 +331,23 @@ class Data:
 
         # Set the index to datetime
         base_df = base_df.set_index("datetime")
-        base_df = self._add_index_columns_from_datetime(base_df)
 
         # Aggregate the input time_series based on time_period
 
         group_by_columns = self._find_groupby_columns()
 
-        grouped_df = base_df.groupby(group_by_columns)[self.settings.LOADSHAPE_TYPE]
+        base_df = base_df.groupby(group_by_columns)[self.settings.LOADSHAPE_TYPE]
 
-        agg_df = grouped_df.agg(loadshape=self.settings.AGG_TYPE).reset_index()
+        base_df = base_df.agg(loadshape=self.settings.AGG_TYPE).reset_index()
 
         # Sort the values so that the ordering is maintained correctly
-        agg_df = agg_df.sort_values(by=group_by_columns)
+        base_df = base_df.sort_values(by=group_by_columns)
 
         # Create the count of the index per ID
-        agg_df["time"] = agg_df.groupby("id").cumcount() + 1
+        base_df["time"] = base_df.groupby("id").cumcount() + 1
 
         # Validate that all the values are correct
-        loadshape_df = self._validate_format_loadshape(agg_df)
+        loadshape_df = self._validate_format_loadshape(base_df)
 
         return loadshape_df
 
@@ -355,6 +380,8 @@ class Data:
 
 
         """
+
+        # TODO : we should accept loadshapes in unstacked version as well
         if loadshape_df is None and time_series_df is None and features_df is None:
             raise ValueError(
                 "A loadshape, time series, or features dataframe must be provided."
@@ -365,9 +392,17 @@ class Data:
                 "Both loadshape dataframe and time series dataframe are provided. Please provide only one."
             )
 
+        if self.settings.TIME_PERIOD is not None and (loadshape_df is not None or time_series_df is None):
+            # Time period should only be set if a time series dataframe is provided
+            raise ValueError(
+                "Time period is set, but no time series dataframe is provided. Please provide a time series dataframe."
+            )
+
         if loadshape_df is not None:
-            if self.loadshape is not None:
+            if self.loadshape is not None :
                 raise ValueError("Loadshape Data has already been set.")
+            elif self.settings.LOADSHAPE_TYPE is not None:
+                raise ValueError("Loadshape Type cannot be set for a loadshape dataframe.")
 
             loadshape_df = self._validate_format_loadshape(loadshape_df)
 
@@ -388,8 +423,10 @@ class Data:
                 ~loadshape_df.index.isin(self.excluded_ids["id"])
             ]
 
-        self.features = features_df
-        self.loadshape = loadshape_df
+        # If the dataframes are empty return None, not an empty dataframe
+        if features_df is not None:
+            self.features = features_df if not features_df.empty else None
+        self.loadshape = loadshape_df if not loadshape_df.empty else None
 
         return self
 
@@ -422,8 +459,13 @@ if __name__ == "__main__":
     # Convert 'datetime' column to datetime type
     df["datetime"] = pd.to_datetime(df["datetime"])
 
-    settings = Data_Settings(TIME_PERIOD=_const.TimePeriod.DAY_OF_WEEK)
+    settings = Data_Settings(TIME_PERIOD=_const.TimePeriod.HOURLY_MONTH)
 
     data = Data(None).set_data(time_series_df=df)
+    start_time = time.time()
     data = Data(settings).set_data(time_series_df=df)
+    end_time = time.time()
     print(data.loadshape)
+
+    execution_time = end_time - start_time
+    print(f"The command took {execution_time} seconds to execute.")
