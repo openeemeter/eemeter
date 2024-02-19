855918013cb0ec5ec82619f6ef127e5cb93ecb0d
travis-recurve <124101151+travis-recurve@users.noreply.github.com>
2024-02-19 21:24:22 +0000

    daily data bug
    Signed-off-by: travis-recurve <124101151+travis-recurve@users.noreply.github.com>
    
    Former-commit-id: 077a7d8b1948e7d2f4cb2cea896713be7af8a69d

--- eemeter/common/test_data.py
+++ eemeter/common/test_data.py
@@ -19,13 +19,19 @@
 """
 
 import pandas as pd
+import requests
 from pathlib import Path
 
 from eemeter.common.const import TutorialDataChoice
 
 # Define the current directory
-current_dir = Path(__file__).parent
+current_dir = Path(__file__).resolve().parent
 data_dir = current_dir.parents[1] / "data"
+
+# Set download branch
+## TODO: THIS BRANCH MUST BE UPDATED TO THE CURRENT BRANCH
+branch = "feature/caltrack-2.1-daily"
+
 
 def load_test_data(data_type: str):
     """Returns back tutorial data of the given data type as a dataframe
@@ -53,33 +59,97 @@ def load_test_data(data_type: str):
         raise ValueError(f"Data type {data_type} not recognized. \nMust be one of {keys}.")
 
     if data_type == TutorialDataChoice.FEATURES:
-        df = pd.read_csv(data_dir / "features.csv")
+        df = _load_file("features.csv")
     
     elif data_type == TutorialDataChoice.SEASONAL_HOUR_DAY_WEEK_LOADSHAPE:
-        df = pd.read_csv(data_dir / "seasonal_hourly_day_of_week_loadshape.csv")
+        df = _load_file("seasonal_hourly_day_of_week_loadshape.csv")
     
     elif data_type == TutorialDataChoice.SEASONAL_DAY_WEEK_LOADSHAPE:
-        df = pd.read_csv(data_dir / "seasonal_day_of_week_loadshape.csv")
+        df = _load_file("seasonal_day_of_week_loadshape.csv")
     
     elif data_type == TutorialDataChoice.MONTH_LOADSHAPE:
-        df = pd.read_csv(data_dir / "month_loadshape.csv")
+        df = _load_file("month_loadshape.csv")
     
     elif data_type == TutorialDataChoice.HOURLY_COMPARISON_GROUP_DATA:
         df = pd.concat(
             [
-                pd.read_parquet(data_dir / "hourly_data_0.parquet"), 
-                pd.read_parquet(data_dir / "hourly_data_1.parquet")
+                _load_file("hourly_data_0.parquet"),
+                _load_file("hourly_data_1.parquet")
             ], 
             axis=0
         )
 
     elif data_type == TutorialDataChoice.HOURLY_TREATMENT_DATA:
-        df = pd.read_parquet(data_dir / "hourly_data_2.parquet")
+        df = _load_file("hourly_data_2.parquet")
     
     if data_type not in [TutorialDataChoice.HOURLY_COMPARISON_GROUP_DATA, TutorialDataChoice.HOURLY_TREATMENT_DATA]:
         df = df.set_index("id")
     
     return df
+
+
+def download_all_test_data():
+    """Downloads all the tutorial data to the data directory"""
+
+    # get all repo files
+    repo_full_name = 'openeemeter/eemeter'
+    path = 'data'
+
+    url = f"https://api.github.com/repos/{repo_full_name}/contents/{path}"
+
+    if branch != "master":
+        url += f"?ref={branch}"
+
+    r = requests.get(url)
+    r.raise_for_status()
+
+    files = [file['name'] for file in r.json() if file['type'] == 'file']
+
+    # download all repo files
+    for file in files:
+        _download_repo_data_file(file)
+
+
+def _load_file(file):
+    attribution_file = data_dir / "attribution.txt"
+    file = data_dir / file
+    ext = file.suffix
+
+    # if file does not exist, download it
+    if not file.exists():
+        # always check for attribution file
+        if not attribution_file.exists():
+           _download_repo_data_file(attribution_file)
+
+        _download_repo_data_file(file)
+
+    if ext == ".csv":
+        df = pd.read_csv(file)
+    
+    elif ext == ".parquet":
+        df = pd.read_parquet(file)
+    
+    else:
+        raise ValueError(f"File type {ext} not recognized.")
+
+    return df
+
+
+def _download_repo_data_file(file):
+    repo_full_name = 'openeemeter/eemeter'
+    path = 'data'
+
+    url = f"https://raw.githubusercontent.com/{repo_full_name}/{branch}/{path}/{file}"
+
+    r = requests.get(url)
+    r.raise_for_status()
+
+    # make directory if it doesn't exist
+    if not data_dir.exists():
+        data_dir.mkdir()
+
+    with open(data_dir / file, 'wb') as f:
+        f.write(r.content)
 
 
 if __name__ == "__main__":
