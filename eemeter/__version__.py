f92fe72a1a2a20919bff5062942f102f3ae77e3b
Jason Chulock <jason@recurve.com>
2024-08-27 21:44:58 +0000

    Bump alpha version
    Signed-off-by: Jason Chulock <jason@recurve.com>
    
    Former-commit-id: 4da7ba060f2c5a3392795ba64e96e39331813ecc

--- eemeter/__version__.py
+++ eemeter/__version__.py
@@ -20,7 +20,7 @@
 __title__ = "eemeter"
 __description__ = "Open Energy Efficiency Meter"
 __url__ = "http://github.com/openeemeter/eemeter"
-__version__ = "4.0.6"
+__version__ = "4.1.0a1"
 __author__ = "Phil Ngo"
 __author_email__ = "admin@openee.io"
 __license__ = "Apache 2.0"
