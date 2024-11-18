369783e7f4a61b30a76338844642a3a934668c2a
travis-recurve <124101151+travis-recurve@users.noreply.github.com>
2024-11-18 21:26:39 +0000

    temporal bins explained variance change
    Signed-off-by: travis-recurve <124101151+travis-recurve@users.noreply.github.com>
    
    Former-commit-id: 33ef36c43d8124a0bf8f6a51dc043e6ac558df10

--- eemeter/eemeter/models/hourly/settings.py
+++ eemeter/eemeter/models/hourly/settings.py
@@ -185,7 +185,7 @@ class TemporalClusteringSettings(BaseSettings):
 
     """minimum variance ratio for PCA clustering"""
     PCA_MIN_VARIANCE_RATIO_EXPLAINED: float = pydantic.Field(
-        default=0.95,
+        default=0.8,
         ge=0.5,
         le=1,
     )
