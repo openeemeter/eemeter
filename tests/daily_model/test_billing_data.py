668fe10c84896141141e4534ffc7dde76741159a
Stephen Suffian <stephen.suffian@pm.me>
2024-12-20 11:43:16 -0500

    Replace 'T' with 'min' for pandas frequencies
    Signed-off-by: Stephen Suffian <stephen.suffian@pm.me>
    
    Former-commit-id: 190b506fd11552281d17b51abe3b45a77e1a871d

--- tests/daily_model/test_billing_data.py
+++ tests/daily_model/test_billing_data.py
@@ -55,7 +55,7 @@ def get_datetime_index_half_hourly_with_timezone():
         start="2023-01-01",
         end="2024-01-01",
         inclusive="left",
-        freq="30T",
+        freq="30min",
         tz="US/Eastern",
     )
 
@@ -546,7 +546,7 @@ def test_billing_baseline_data_with_specific_monthly_input():
 
 
 @pytest.mark.parametrize(
-    "get_datetime_index", [["30T", True], ["h", True]], indirect=True
+    "get_datetime_index", [["30min", True], ["h", True]], indirect=True
 )
 def test_billing_reporting_data_with_missing_half_hourly_frequencies(
     get_datetime_index,
@@ -573,7 +573,7 @@ def test_billing_reporting_data_with_missing_half_hourly_frequencies(
     assert cls.df is not None
     assert len(cls.df) == NUM_DAYS_IN_YEAR
 
-    if datetime_index.freq == "30T":
+    if datetime_index.freq == "30min":
         assert len(cls.df.temperature.dropna()) == 268
     elif datetime_index.freq == "h":
         assert len(cls.df.temperature.dropna()) == 270
