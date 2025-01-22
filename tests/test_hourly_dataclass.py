1817e8f2e37ee6d24edfd703db8b0d8a0ceb4f72
Jason Chulock <jason@recurve.com>
2025-01-22 18:56:26 -0500

    Add hourly data tests
    Signed-off-by: Jason Chulock <jason@recurve.com>
    
    Former-commit-id: d61931aa3b3bb8a760d9eaef9060f761d5c5016c

--- tests/test_hourly_dataclass.py
+++ tests/test_hourly_dataclass.py
@@ -19,11 +19,14 @@
 """
 from datetime import datetime
 
-from eemeter.eemeter import HourlyBaselineData, HourlyReportingData, HourlyModel
+from eemeter.eemeter import HourlyBaselineData, HourlyReportingData, HourlyModel, HourlySolarSettings, HourlyNonSolarSettings
+from eemeter.eemeter.common.exceptions import DataSufficiencyError
+from eemeter.eemeter.common.warnings import EEMeterWarning
 from eemeter.common.test_data import load_test_data
 import numpy as np
 import pandas as pd
 import pytest
+from math import ceil
 
 _TEST_METER = 110596
 
@@ -33,9 +36,26 @@ def hourly_data():
     baseline, reporting = load_test_data("hourly_treatment_data")
     return baseline.loc[_TEST_METER], reporting.loc[_TEST_METER]
 
+@pytest.fixture
+def baseline(hourly_data):
+    baseline, _ = hourly_data
+    baseline.loc[baseline["observed"] > 513, "observed"] = 0  #quick extreme value removal
+    baseline["ghi"] = (np.sin(np.linspace(0, 2*np.pi*len(baseline), len(baseline))) * 40).round(2) + 40
+    return baseline
+    
+@pytest.fixture
+def reporting(hourly_data):
+    _, reporting = hourly_data
+    reporting["ghi"] = (np.sin(np.linspace(0, 2*np.pi*len(reporting), len(reporting))) * 40).round(2) + 40
+    return reporting
 
-def test_one(hourly_data):
-    baseline, reporting = hourly_data
+@pytest.fixture
+def baseline_ghi(baseline):
+    #generate ghi as a sin wave daily period peaking afternoon
+    baseline["ghi"] = np.sin(np.linspace(0, 2*np.pi, len(baseline)))
+    return baseline
+
+def test_good_data(baseline, reporting):
     baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
     reporting_data = HourlyReportingData(reporting, is_electricity_data=True)
     hm = HourlyModel().fit(baseline_data)
@@ -45,14 +65,135 @@ def test_one(hourly_data):
     p2 = hm2.predict(reporting_data)
     assert p1.equals(p2)
 
-def test_unaligned_data(hourly_data):
-    baseline, reporting = hourly_data
+def test_misaligned_data(baseline, reporting):
     reporting.index = reporting.index.shift(8, freq="H")
     baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
     reporting_data = HourlyReportingData(reporting, is_electricity_data=True)
     hm = HourlyModel().fit(baseline_data)
     hm.predict(reporting_data)
 
+def test_tz_naive(baseline):
+    baseline.index = baseline.index.tz_localize(None)
+    with pytest.raises(ValueError):
+        HourlyBaselineData(baseline, is_electricity_data=True)
+
+def test_tz_mismatch(baseline):
+    # might allow automatic adjustment from the model in the future, but hard requirement for now
+    baseline.index = baseline.index.tz_convert("US/Pacific")
+    reporting = baseline.copy()
+    reporting.index = reporting.index.tz_convert("US/Eastern")
+    baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
+    reporting_data = HourlyReportingData(reporting, is_electricity_data=True)
+    hm = HourlyModel().fit(baseline_data)
+    with pytest.raises(ValueError):
+        hm.predict(reporting_data)
+
+def test_predict_missing_fit_features(baseline, reporting):
+    baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
+    hm = HourlyModel(settings=HourlySolarSettings()).fit(baseline_data)
+    reporting.drop("ghi", axis=1, inplace=True)
+    reporting_data = HourlyReportingData(reporting, is_electricity_data=True)
+    with pytest.raises(ValueError):
+        hm.predict(reporting_data)
+
+def test_nonsolar_predict_with_ghi(baseline, reporting, caplog):
+    baseline.drop("ghi", axis=1, inplace=True)
+    baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
+    hm = HourlyModel().fit(baseline_data)
+    reporting_data = HourlyReportingData(reporting, is_electricity_data=True)
+    with caplog.at_level("WARNING"):
+        hm.predict(reporting_data)
+        assert "GHI" in caplog.text
+
+def test_forced_solar_model_fit_no_ghi(baseline):
+    baseline = baseline.drop("ghi", axis=1)
+    baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
+    with pytest.raises(ValueError):
+        HourlyModel(settings=HourlySolarSettings()).fit(baseline_data)
+
+def test_forced_nonsolar_model_fit_with_ghi(baseline):
+    baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
+    hm = HourlyModel(settings=HourlyNonSolarSettings()).fit(baseline_data)
+    assert [w for w in hm.warnings if w.qualified_name == "eemeter.potential_model_mismatch"]
+
+def test_no_data(baseline):
+    baseline["observed"] = 0
+    baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
+    with pytest.raises(DataSufficiencyError):
+        HourlyModel().fit(baseline_data)
+
+def test_negative_meter_values(baseline):
+    baseline.loc["2018-01-08", "observed"] = -1
+
+    # gas data can't be negative
+    baseline_data = HourlyBaselineData(baseline, is_electricity_data=False)
+    with pytest.raises(DataSufficiencyError):
+        HourlyModel().fit(baseline_data)
+
+    # elec can
+    baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
+    HourlyModel().fit(baseline_data)
+
+def test_invalid_baseline_lengths(baseline):
+    #TODO import min/max length from constants
+    MAX_BASELINE_HOURS = 8760
+    MIN_BASELINE_HOURS = ceil(MAX_BASELINE_HOURS * 0.9) - 24
+    short_df = baseline.iloc[:MIN_BASELINE_HOURS]
+
+    extra_day = baseline.iloc[-24:]
+    extra_day.index += pd.Timedelta(days=1)
+    long_df = pd.concat([baseline, extra_day])
+
+    short_baseline = HourlyBaselineData(short_df, is_electricity_data=True)
+    long_baseline = HourlyBaselineData(long_df, is_electricity_data=True)
+    with pytest.raises(DataSufficiencyError):
+        HourlyModel().fit(short_baseline)
+    hm_short = HourlyModel().fit(short_baseline, ignore_disqualification=True)
+    with pytest.raises(DataSufficiencyError):
+        HourlyModel().fit(long_baseline)
+    hm_long = HourlyModel().fit(long_baseline, ignore_disqualification=True)
+
+def test_low_freq_temp(baseline):
+    baseline["temperature"] = baseline["temperature"].resample('D').mean()
+    baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
+    assert_dq(baseline_data, ["eemeter.sufficiency_criteria.too_many_days_with_missing_temperature_data"])
+    with pytest.raises(DataSufficiencyError):
+        HourlyModel().fit(baseline_data)
+
+def test_low_freq_meter(baseline):
+    baseline["observed"] = baseline["observed"].resample('D').mean()
+    baseline_data = HourlyBaselineData(baseline, is_electricity_data=True)
+    assert_dq(baseline_data, ["eemeter.sufficiency_criteria.too_many_days_with_missing_meter_data"])
+    with pytest.raises(DataSufficiencyError):
+        HourlyModel().fit(baseline_data)
+
+def test_monthly_percentage(baseline):
+    missing_idx = pd.date_range(start=baseline.index.min(), end=baseline.index.max(), freq="H")
+    #create datetimeindex where a little over 10% of days are missing each month
+    missing_idx = missing_idx[missing_idx.day < 4]
+    invalid_baseline = baseline[~baseline.index.isin(missing_idx)]
+    #create datetimeindex where a little under 10% of days are missing each month
+    missing_idx = missing_idx[missing_idx.day < 3]
+    valid_baseline = baseline[~baseline.index.isin(missing_idx)]
+
+    baseline_data = HourlyBaselineData(invalid_baseline, is_electricity_data=True)
+    assert_dq(baseline_data, ["eemeter.sufficiency_criteria.missing_monthly_temperature_data"])
+    with pytest.raises(DataSufficiencyError):
+        HourlyModel().fit(baseline_data)
+    baseline_data = HourlyBaselineData(valid_baseline, is_electricity_data=True)
+    HourlyModel().fit(baseline_data)
+
+def test_hourly_consecutive_missing(baseline):
+    pass
+
+def assert_dq(data, expected_disqualifications):
+    remaining_dq = set(expected_disqualifications)
+    for dq in data.disqualification:
+        if dq.qualified_name in remaining_dq:
+            remaining_dq.remove(dq.qualified_name)
+    assert not remaining_dq
+    
+    
 
 """TEST CASES
 TODO get a couple example meters with GHI, potentially some supplemental features?
